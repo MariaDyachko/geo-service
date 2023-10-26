2b5f93e49975ecc69c97047746736c272fcd190a
Maria Dyachko <up_11_14@mail.ru>
2023-10-26 23:11:50 +0300

    second commit

--- src/test/java/ru/netology/sender/MessageSenderImplTest.java
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -1,20 +1,23 @@
 package ru.netology.sender;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
+import ru.netology.entity.Location;
 import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationServiceImpl;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 class MessageSenderImplTest {
 
-    //@org.junit.jupiter.api.Test
-    @Test
-    void send() {
+    @DisplayName("sender")
+    @org.junit.jupiter.api.Test
+    void sendMessage() {
 
         LocalizationServiceImpl loc = new LocalizationServiceImpl();
         GeoServiceImpl geoService = new GeoServiceImpl();
@@ -22,11 +25,27 @@ class MessageSenderImplTest {
 
         Map<String, String> headers = new HashMap<String, String>();
         headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.123.12.19");
-        Mockito.when(sender.send(headers)).thenReturn("saas");
+        Mockito.when(sender.send(headers)).thenReturn("Добро пожаловать");
 
-        //String actual = sender.send(headers);
+        String actual = sender.send(headers);
+        assertEquals(actual, "Добро пожаловать");
 
-        //assertEquals(actual, "saas");
+        headers.put(MessageSenderImpl.IP_ADDRESS_HEADER, "96.123.12.19");
+        Mockito.when(sender.send(headers)).thenReturn("Welcome");
+        //
 
+    }
+    @DisplayName("geoLocation")
+    @Test
+    void tryLocation(){
+        var geo = Mockito.mock(GeoServiceImpl.class);
+        Mockito.when(geo.byIp("96.123.12.19")).thenReturn(new Location("New York", Country.USA, null,  0));
+    }
+
+    @DisplayName("location")
+    @Test
+    void localeTest(){
+        var loc = Mockito.mock(LocalizationServiceImpl.class);
+        Mockito.when(loc.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
     }
 }
